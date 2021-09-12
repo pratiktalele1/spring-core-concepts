8e60c77569158ee6a0ce23d03d031f52d4a4e964
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-12 21:39:30 +0530

    commiting UC2

--- src/main/java/com/bridgelabz/app/SpringCoreConceptsApplication.java
+++ src/main/java/com/bridgelabz/app/SpringCoreConceptsApplication.java
@@ -1,14 +1,25 @@
 package com.bridgelabz.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringCoreConceptsApplication {
+	
+//	UC2 adding logger for project
+	private static final Logger logger= LoggerFactory.getLogger(SpringCoreConceptsApplication.class);
 
 	public static void main(String[] args) {
+//	UC1 case
 		SpringApplication.run(SpringCoreConceptsApplication.class, args);
 		System.out.println("Hello from SpringCoreConcepts");
+		
+//		adding logger message
+		logger.info("spring core concepts from logger");
+
 	}
 
 }
