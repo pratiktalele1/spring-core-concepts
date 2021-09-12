5f43ff44f207881db71693895cf034dbafa3f375
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-12 21:26:56 +0530

    commiting spring-core-concepts

---
+++ src/main/java/com/bridgelabz/app/SpringCoreConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreConceptsApplication.class, args);
+		System.out.println("Hello from SpringCoreConcepts");
+	}
+
+}
